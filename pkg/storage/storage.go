43a0bf0105f42dd6919ebb0cc86bd230c540c0a8
Andrey <andre-vylegzhani@yandex.ru>
2023-05-13 19:15:30 +0300

    -v2

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -1,6 +1,6 @@
 package storage
 
-import "github.com/Andrey-VN/storage/internal/storage"
+import "github.com/Andrey-VN/storage/v2/internal/storage"
 
 func NewStorage() *storage.Storage {
 	return storage.NewStorage()
